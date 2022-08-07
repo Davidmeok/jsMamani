ea8bc4cb79f7301441476ed787b13028e02d8243
Davidmeok <davidcatrielmamaniescalera@gmail.com>
2022-08-07 19:49:35 -0300

    arreglo de imagenes

--- js/complementario3.js
+++ js/complementario3.js
@@ -6,7 +6,7 @@ let i = 0; // INCREMENTO A USAR PARA LOS NODELIST
 // PARA CADA ELEMENTO ELEMENTO DEL ARRAY PRODUCTOS
 productos.forEach((elementos) => {
     // CREAR Y AGREGAR A LAS CLASES SECTION-ART
-    sArt[i++].innerHTML = `<img src="../img/crismalite.png" alt="">
+    sArt[i++].innerHTML = `<img src="./img/crismalite.png" alt="">
                     <h2 class="section-tittle">${elementos.nombre} <span>$${elementos.precio}</span></h2>
                     <div class="con-btn">
                         <button class="btn btn-info">Comprar</button>
